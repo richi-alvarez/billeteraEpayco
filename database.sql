d6a26aee5402ae9cf71511b21f15ce1d2396d6cf
pruebasepayco <ricardo.saldarriaga@payco.co>
2020-06-01 18:00:25 -0500

    second commit

--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
     name varchar(150) not null,
     documento int(150),
     email varchar(255) not null,
-    celular int(255) not null,
+    celular varchar(255) not null,
     password varchar(255) not null,
     created_at datetime DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_users PRIMARY KEY (id)
@@ -16,6 +16,8 @@ CREATE TABLE whallet(
     id  int(255) auto_increment not null,
     user_id int(150) not null,
     saldo int(150),
+    token varchar(255),
+    session varchar(255),
     created_at datetime DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_whallet PRIMARY KEY (id),
     CONSTRAINT fk_whallet_user FOREIGN KEY(user_id) REFERENCES users(id)
